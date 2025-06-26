f114f6597619d4f120a17b610c1b1c792e8ca9ce
yohannd1 <mitonanan12@gmail.com>
2025-06-26 18:12:05 -0300

    numerical FDT organization

--- fdth/numerical_fdt.py
+++ fdth/numerical_fdt.py
@@ -1,4 +1,4 @@
-from typing import Literal, Sequence
+from typing import Optional, Literal, Sequence, Any
 from dataclasses import dataclass
 from functools import lru_cache
 
@@ -13,15 +13,35 @@ NumericalBin = Literal["Sturges", "Scott", "FD"]
 
 @dataclass
 class BreaksInfo:
-    pass
+    start: float
+    end: float
+    h: float
+    k: int
+    right: bool
+    bins: np.array
 
 
 class NumericalFDT(FrequencyDistribution):
     """Stores information about a numerical frequency distribution, and allows related operations."""
 
+    table: pd.DataFrame
+    """The inner frequency distribution table. Columns:
+        - "Class limits": the limits of the class.
+        - "f": the absolute frequency of each class.
+        - "rf": the relative frequency of each class.
+        - "rf(%)": the relative frequency expressed as a percentage.
+        - "cf": the cumulative absolute frequency.
+        - "cf(%)": the cumulative relative frequency expressed as a percentage.
+    """
+
+    breaks_info: BreaksInfo
+    """Information about the binning done in the creation of the FDT."""
+
     def __init__(
         self,
-        data: pd.Series | list | np.ndarray,
+        data: Optional[pd.Series | list | np.ndarray] = None,
+        *,
+        freqs: Optional[pd.Series | dict] = None,
         k: int | None = None,
         start: float | None = None,
         end: float | None = None,
@@ -30,59 +50,70 @@ class NumericalFDT(FrequencyDistribution):
         right: bool = False,
         na_rm: bool = False,
     ):
-        if isinstance(data, (list, np.ndarray)):
-            self._data = pd.Series(data)
-        elif isinstance(data, pd.Series):
-            self._data = data
+        if data is not None:
+            if freqs is not None:
+                raise ValueError("`data` and `freqs` must not be both specified")
+
+            if isinstance(data, (list, np.ndarray)):
+                data = pd.Series(data)
+            elif isinstance(data, pd.Series):
+                pass
+            else:
+                raise TypeError("`data` must be list | pandas.Series | numpy.ndarray")
+
+            self.count = len(data)
+            self.table, self.breaks_info = self._fdt_numeric_simple(
+                data,
+                k=k,
+                start=start,
+                end=end,
+                h=h,
+                breaks=breaks,
+                right=right,
+                na_rm=na_rm,
+            )
+        elif freqs is not None:
+            if data is not None:
+                raise ValueError("`data` and `freqs` must not be both specified")
+
+            if isinstance(freqs, dict):
+                freqs = pd.Series(freqs)
+            elif isinstance(freqs, pd.Series):
+                pass
+            else:
+                raise TypeError("`data` must be dict | pandas.Series")
+
+            raise NotImplementedError("TODO")
         else:
-            raise TypeError("Data must be a list, a pandas.Series or an numpy.ndarray")
-
-        self._data_size = len(self._data)
-
-        result = _fdt_numeric_simple(
-            self._data,
-            k=k,
-            start=start,
-            end=end,
-            h=h,
-            breaks=breaks,
-            right=right,
-            na_rm=na_rm,
-        )
-
-        self.table = result["table"]
-        """The inner frequency distribution table."""
-
-        self.breaks_info = result["breaks"]
-        """Information about the binning done in the creation of the FDT."""
+            raise ValueError("one of `data` or `table` must be specified")
 
     @lru_cache(maxsize=1)
     def mean(self) -> float:
         """Calculate an approximate of the mean of the data represented by the FDT."""
 
-        start = self.breaks_info["start"]
-        end = self.breaks_info["end"]
-        h = self.breaks_info["h"]
+        start = self.breaks_info.start
+        end = self.breaks_info.end
+        h = self.breaks_info.h
 
         # define class interval
         breaks = np.arange(start, end + h, h)
 
         # calculate midpoints of the class intervals
-        mids = 0.5 * (breaks[:-1] + breaks[1:])
+        midpoints = 0.5 * (breaks[:-1] + breaks[1:])
 
         # frequencies of each class
         y = self.table.loc[:, "f"]
 
         # return the weighted mean of the midpoints
-        return np.sum(y * mids) / np.sum(y)
+        return np.sum(y * midpoints) / np.sum(y)
 
     @lru_cache(maxsize=1)
     def median(self) -> float:
         """Calculate an approximate of the median (50th percentile) of the data represented by the FDT."""
 
-        start = self.breaks_info["start"]
-        end = self.breaks_info["end"]
-        h = self.breaks_info["h"]
+        start = self.breaks_info.start
+        end = self.breaks_info.end
+        h = self.breaks_info.h
 
         # Número total de observações
         n = self.table.iloc[-1, 4]
@@ -110,20 +141,20 @@ class NumericalFDT(FrequencyDistribution):
     def var(self) -> float:
         """Calculate an approximate of the variance of the data represented by the FDT."""
 
-        start = self.breaks_info["start"]
-        end = self.breaks_info["end"]
-        h = self.breaks_info["h"]
+        start = self.breaks_info.start
+        end = self.breaks_info.end
+        h = self.breaks_info.h
 
         # Definir intervalos de classe com base nos valores 'start', 'end' e 'h'
         breaks = np.arange(start, end + h, h)
 
         # Calcular pontos médios dos intervalos de classe
-        mids = 0.5 * (breaks[:-1] + breaks[1:])
+        midpoints = 0.5 * (breaks[:-1] + breaks[1:])
 
         # Frequências das classes
         y = self.table.loc[:, "f"]
 
-        return np.sum((mids - self.mean()) ** 2 * y) / (np.sum(y) - 1)
+        return np.sum((midpoints - self.mean()) ** 2 * y) / (np.sum(y) - 1)
 
     @lru_cache(maxsize=1)
     def sd(self) -> float:
@@ -133,15 +164,14 @@ class NumericalFDT(FrequencyDistribution):
     @lru_cache(maxsize=1)
     def mfv(self) -> pd.Series:
         """Returns the most frequent values (modes) of the data set."""
-        return self._data.mode()
+        raise NotImplementedError("TODO")
 
-    def get_table(self):
-        # FIXME: deprecate in favor of `self.table`
+    def get_table(self) -> pd.DataFrame:
         return self.table
 
     def plot_histogram(self) -> None:
         # FIXME: whoops. I think I (yohanan) messed this up
-        plt.hist(self.table, bins=self.breaks_info["bins"], edgecolor="black")
+        plt.hist(self.table, bins=self.breaks_info.bins, edgecolor="black")
         plt.title("Histograma")
         plt.xlabel("Valor")
         plt.ylabel("Frequência")
@@ -169,114 +199,122 @@ class NumericalFDT(FrequencyDistribution):
         return table.to_string(index=row_numbers, justify="right" if right else "left")
 
     def __repr__(self) -> str:
-        count = self._data_size
-
-        res = f"NumericalFDT ({count} elements, {self.breaks_info['k']} classes, amplitude of {self.breaks_info['h']:.2f}):\n"
+        res = f"NumericalFDT ({self.count} elements, {self.breaks_info.k} classes, amplitude of {self.breaks_info.h:.2f}):\n"
         res += self.to_string(max_lines=5) + "\n"
-        if count > 5:
-            res += f"... {count-5} more lines"
+        if self.count > 5:
+            res += f"... {self.count-5} more lines"
 
         return res
 
+    @staticmethod
+    def _fdt_numeric_simple(data, k, start, end, h, breaks, right, na_rm) -> tuple[pd.DataFrame, BreaksInfo]:
+        data = np.array([np.nan if v is None else v for v in data], dtype=np.float64)
 
-def _fdt_numeric_simple(x, k, start, end, h, breaks, right, na_rm):
-    x = np.array([np.nan if v is None else v for v in x], dtype=np.float64)
+        if not np.issubdtype(data.dtype, np.number):
+            raise TypeError("The data vector must be numeric.")
 
-    if not np.issubdtype(x.dtype, np.number):
-        raise TypeError("The data vector must be numeric.")
+        if na_rm:
+            data = data[~np.isnan(data)]
+        elif np.any(np.isnan(data)):
+            raise ValueError("The data has <NA> values and na.rm=FALSE by default.")
 
-    if na_rm:
-        x = x[~np.isnan(x)]
-    elif np.any(np.isnan(x)):
-        raise ValueError("The data has <NA> values and na.rm=FALSE by default.")
+        # Bin calculation based on specified method
+        if k is None and start is None and end is None and h is None:
+            if breaks == "Sturges":
+                k = int(np.ceil(1 + 3.322 * np.log10(len(data))))
+            elif breaks == "Scott":
+                std_dev = np.std(data)
+                k = int(
+                    np.ceil(
+                        (data.max() - data.min())
+                        / (3.5 * std_dev / (len(data) ** (1 / 3)))
+                    )
+                )
+            elif breaks == "FD":
+                iqr = np.percentile(data, 75) - np.percentile(data, 25)
+                k = int(
+                    np.ceil(
+                        (data.max() - data.min()) / (2 * iqr / (len(data) ** (1 / 3)))
+                    )
+                )
+            else:
+                raise ValueError("Invalid 'breaks' method.")
 
-    # Bin calculation based on specified method
-    if k is None and start is None and end is None and h is None:
-        if breaks == "Sturges":
-            k = int(np.ceil(1 + 3.322 * np.log10(len(x))))
-        elif breaks == "Scott":
-            std_dev = np.std(x)
-            k = int(
-                np.ceil((x.max() - x.min()) / (3.5 * std_dev / (len(x) ** (1 / 3))))
+            start, end = (
+                data.min() - abs(data.min()) / 100,
+                data.max() + abs(data.max()) / 100,
             )
-        elif breaks == "FD":
-            iqr = np.percentile(x, 75) - np.percentile(x, 25)
-            k = int(np.ceil((x.max() - x.min()) / (2 * iqr / (len(x) ** (1 / 3)))))
+            R = end - start
+            h = R / k
+
+        elif start is None and end is None and h is None:
+            start, end = (
+                data.min() - abs(data.min()) / 100,
+                data.max() + abs(data.max()) / 100,
+            )
+            R = end - start
+            h = R / k
+
+        elif k is None and h is None:
+            R = end - start
+            k = int(np.sqrt(abs(R)))
+            k = max(k, 5)
+            h = R / k
+
+        elif k is None:
+            pass
+
         else:
-            raise ValueError("Invalid 'breaks' method.")
+            raise ValueError("Please check the function syntax!")
 
-        start, end = x.min() - abs(x.min()) / 100, x.max() + abs(x.max()) / 100
-        R = end - start
-        h = R / k
+        # Generate the frequency distribution table
+        table, bins = NumericalFDT._make_fdt_simple(data, start, end, h, right)
 
-    elif start is None and end is None and h is None:
-        start, end = x.min() - abs(x.min()) / 100, x.max() + abs(x.max()) / 100
-        R = end - start
-        h = R / k
+        breaks_info = BreaksInfo(
+            start=start,
+            end=end,
+            h=h,
+            k=k,
+            right=int(right),
+            bins=bins,
+        )
 
-    elif k is None and h is None:
-        R = end - start
-        k = int(np.sqrt(abs(R)))
-        k = max(k, 5)
-        h = R / k
+        return table, breaks_info
 
-    elif k is None:
-        pass
+    @staticmethod
+    def _make_fdt_simple(
+        data: pd.Series, start: float, end: float, h: float, right: bool = False
+    ) -> pd.DataFrame:
+        """Create a simple frequency distribution table.
 
-    else:
-        raise ValueError("Please check the function syntax!")
+        :param data: The data array.
+        :param start: The starting point of the distribution range.
+        :param end: The endpoint of the distribution range.
+        :param h: The class interval width.
+        :param right: Whether to include the right endpoint in each interval.
 
-    # Generate the frequency distribution table
-    table, bins = _make_fdt_simple(x, start, end, h, right)
-    breaks_info = {
-        "start": start,
-        "end": end,
-        "h": h,
-        "k": k,
-        "right": int(right),
-        "bins": bins,
-    }
-    result = {"table": table, "breaks": breaks_info}
+        :return a frequency distribution table with class limits, frequencies, relative frequencies, cumulative frequencies, and cumulative percentages.
+        """
+        bins = np.arange(start, end + h, h)
+        labels = [
+            f"[{round(bins[i], 2)}, {round(bins[i + 1], 2)})"
+            for i in range(len(bins) - 1)
+        ]
+        f = pd.cut(data, bins=bins, right=right, labels=labels).value_counts()
+        rf = f / len(data)
+        rfp = rf * 100
+        cf = f.cumsum()
+        cfp = (cf / len(data)) * 100
 
-    return result
+        table = pd.DataFrame({
+            "Class limits": labels,
+            "f": f.values,
+            "rf": rf.values,
+            "rf(%)": rfp.values,
+            "cf": cf.values,
+            "cf(%)": cfp.values,
+        }) # fmt: skip
 
+        table.index = np.arange(1, len(table) + 1)
 
-def _make_fdt_simple(
-    x: Sequence[float], start: float, end: float, h: float, right: bool = False
-) -> pd.DataFrame:
-    """
-    Create a simple frequency distribution table.
-
-    Parameters:
-    x (array-like): The data array.
-    start (float): The starting point of the distribution range.
-    end (float): The endpoint of the distribution range.
-    h (float): The class interval width.
-    right (bool): Whether to include the right endpoint in each interval.
-
-    Returns:
-    DataFrame: A frequency distribution table with class limits, frequencies, relative frequencies,
-               cumulative frequencies, and cumulative percentages.
-    """
-    bins = np.arange(start, end + h, h)
-    labels = [
-        f"[{round(bins[i], 2)}, {round(bins[i + 1], 2)})" for i in range(len(bins) - 1)
-    ]
-    f = pd.cut(x, bins=bins, right=right, labels=labels).value_counts()
-    rf = f / len(x)
-    rfp = rf * 100
-    cf = f.cumsum()
-    cfp = (cf / len(x)) * 100
-
-    table = pd.DataFrame({
-        "Class limits": labels,
-        "f": f.values,
-        "rf": rf.values,
-        "rf(%)": rfp.values,
-        "cf": cf.values,
-        "cf(%)": cfp.values,
-    }) # fmt: skip
-
-    table.index = np.arange(1, len(table) + 1)
-
-    return table, bins
+        return table, bins
